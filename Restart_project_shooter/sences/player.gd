5c8b97070de86078df259daa982bd415e905b097
22dunkae <22dunkae@ths.school.nz>
2024-07-23 14:44:49 +1200

    added powerup
    added q power up, does not work yet

--- Restart_project_shooter/sences/player.gd
+++ Restart_project_shooter/sences/player.gd
@@ -2,7 +2,7 @@ extends Area2D
 @onready var laser_perfab =preload("res://prefabs/laser.tscn")
 @onready var explosion_prefab= preload("res://prefabs/explosion.tscn")
 signal player_killed
-
+var counter = 0
 signal player_live_lost
 
 
@@ -21,6 +21,16 @@ func _process(delta):
 	if Input.is_action_pressed("player_b") and position.x >50:
 		position.x -=10	
 	
+	if Input.is_action_just_pressed("power_up_q"):
+		counter = 0
+		while counter != 30:
+			var laser = laser_perfab.instantiate()
+			laser.position = position
+			get_parent().add_child(laser)
+			$AudioStreamPlayer.play()
+			counter = counter + 1
+		
+		
 	#laser shooting
 	if Input.is_action_just_pressed("player_shoot"):	
 		#make a laser
